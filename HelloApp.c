1095807079674f38cee69f68ff6d7e089c7276b1
PragmaticPerson <aleksejceresnev43@yandex.ru>
2023-09-10 15:22:16 +0300

    Add work with bin file: write, read

--- HelloApp.c
+++ HelloApp.c
@@ -6,11 +6,15 @@ void printFileContent(FILE*);
 int* createDynamicArray(int);
 void fillArrayWithIndices(int*, int);
 void printArrayRange(int*, int, int);
+void saveArrayToFile(int*, int, const char*);
+void readArrayFromFileAndPrint(const char*);
+long getSizeOfFile(FILE* file);
 
 int main() {
-    char* fileName = "D:\\ParallelProg\\comp_system.txt";
+    char* txtFileName = "comp_system.txt";
+    char* binFileName = "myArray.bin";
     char* mode = "r";
-    FILE* file = openFile(fileName, mode);
+    FILE* file = openFile(txtFileName, mode);
 
     printFileContent(file);
 
@@ -30,6 +34,13 @@ int main() {
 
     printf("Содержимое массива в заданном диапазоне: ");
     printArrayRange(myArray, start, end);
+
+    saveArrayToFile(myArray, size, binFileName);
+
+    printf("Массив сохранен в файле %s.\n", binFileName);
+
+    printf("Чтение из файла и вывод в консоль:\n");
+    readArrayFromFileAndPrint(binFileName);
 
     free(myArray);
     fclose(file);
@@ -80,4 +91,38 @@ void printArrayRange(int* array, int start, int end) {
         printf("%d ", array[i]);
     }
     printf("\n");
+}
+
+void saveArrayToFile(int* array, int size, const char* filename) {
+    char* mode = "wb";
+    FILE* file = openFile(filename, mode);
+
+    fwrite(array, sizeof(int), size, file);
+
+    fclose(file);
+}
+
+void readArrayFromFileAndPrint(const char* filename) {
+    char* mode = "rb";
+    FILE* file = openFile(filename, mode);
+    long size = getSizeOfFile(file);
+    int* array = createDynamicArray(size);
+
+    fread(array, sizeof(int), size, file);
+
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    fclose(file);
+    free(array);
+}
+
+long getSizeOfFile(FILE* file) {
+    fseek(file, 0, SEEK_END);  // Указатель в конец
+    // Размер файла от начала до конца деленый на размер единицы данных
+    long size = ftell(file) / sizeof(int);
+    fseek(file, 0, SEEK_SET);  // Указатель в начало
+    return size;
 }
